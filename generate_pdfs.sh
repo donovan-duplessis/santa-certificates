f75f734f725f6985c49b9824fd9f3b5b11190aaa
Donovan du Plessis <donovan@binarytrooper.com>
2025-12-17 09:15:39 +0200

    Initial commit: Santa Nice List Certificate Generator
    Python script to generate personalized PDF certificates for children
    on Santa's Nice List. Includes festive styling with snowflakes,
    Christmas tree decoration, and wax seal.

---
+++ generate_pdfs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Generate Santa Certificates for Lia and Daniel du Plessis
+
+cd /home/donovan/code/santa
+
+echo "Generating HTML certificates..."
+python3 santa_certificates.py
+
+echo "Converting Lia's certificate to PDF..."
+wkhtmltopdf --enable-local-file-access --page-size A4 --margin-top 0 --margin-bottom 0 --margin-left 0 --margin-right 0 --dpi 300 --print-media-type --disable-smart-shrinking build/lia_certificate.html lia_certificate.pdf
+
+echo "Converting Daniel's certificate to PDF..."
+wkhtmltopdf --enable-local-file-access --page-size A4 --margin-top 0 --margin-bottom 0 --margin-left 0 --margin-right 0 --dpi 300 --print-media-type --disable-smart-shrinking build/daniel_certificate.html daniel_certificate.pdf
+
+echo ""
+echo "Done! PDFs created:"
+echo "  - lia_certificate.pdf"
+echo "  - daniel_certificate.pdf"
